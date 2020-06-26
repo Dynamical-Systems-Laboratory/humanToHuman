a2b27e3694500a6f074df4af94bb0f3c8d9e7fe3
Albert Liu <albertymliu@gmail.com>
2020-06-26 05:20:26 -0400

    formatting mostly

--- analysis/error.py
+++ analysis/error.py
@@ -4,7 +4,6 @@ from scipy.optimize import curve_fit
 from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 import statistics
-import pickle
 
 nexus = 5952679123360942499
 i8 = 7132799880531636771
@@ -39,22 +38,28 @@ all_phones = [
     (i10_i8, 'iPhone 10 & iPhone 8'),
 ]
 
+
 def n_exp(x, a, n):
-    return 10 ** ((x - a) / 10 / n)
+    return 10**((x - a) / 10 / n)
+
 
 def fit_curve_a_n_r2(meters, rssi2d):
     x = [y.mean() for y in rssi2d]
-    (a, n), coef = curve_fit(n_exp, x, meters, p0 = [60,-30], xtol=0.001,
-                             maxfev=100000)
-    a_stdev = np.sqrt(coef[0,0])
-    n_stdev = np.sqrt(coef[1,1])
+    (a, n), coef = curve_fit(n_exp,
+                             x,
+                             meters,
+                             p0=[60, -30],
+                             xtol=0.001,
+                             maxfev=1000000)
+    a_stdev = np.sqrt(coef[0, 0])
+    n_stdev = np.sqrt(coef[1, 1])
 
     r2 = r2_score(n_exp(x, a, n), meters)
 
     return (a, a_stdev), (n, n_stdev), r2
 
 
-def box_plots_normal_ignore_0_with_fit(meters, rssi2d, description, fit = True):
+def box_plots_normal_ignore_0_with_fit(meters, rssi2d, description, fit=True):
     ys = rssi2d[1:]
     Xmin = min(x.min() for x in rssi2d)
     Xmax = max(x.max() for x in rssi2d)
@@ -63,7 +68,7 @@ def box_plots_normal_ignore_0_with_fit(meters, rssi2d, description, fit = True):
     plt.ylabel('Distance (meters)')
     plt.title('Distance vs Measured RSSI ' + description)
 
-    plt.boxplot(rssi2d, positions = meters, vert = False)
+    plt.boxplot(rssi2d, positions=meters, vert=False)
 
     if not fit:
         plt.savefig(f"box_plots_{description}.png")
@@ -81,8 +86,10 @@ def box_plots_normal_ignore_0_with_fit(meters, rssi2d, description, fit = True):
     """.strip().replace('    ', '')
 
     plt.text(-60, 8, text)
+    plt.xscale('log')
     plt.savefig(f"box_plots_{description}.png")
     plt.show()
+
 
 def shaded_area(meters, rssi2d, description):
     Xmin = min(x.mean() for x in rssi2d)
@@ -105,24 +112,37 @@ def shaded_area(meters, rssi2d, description):
 
     xsmall = np.arange(Xmin, Xmax, .001)
     shade_line = n_exp(Xs, a, n)
-    plt.fill_between(Xs, Xs_stdev + shade_line, -Xs_stdev + shade_line,
-                     color = ('.6', '.6','.6'))
-    plt.plot(xsmall, n_exp(xsmall, a, n), color = 'b', marker=',')
-    plt.plot(Xs, meters, mfc = 'k', mec='k', linestyle='None', marker='x')
+    plt.fill_between(Xs,
+                     shade_line + Xs_stdev,
+                     shade_line - Xs_stdev,
+                     color=('.6', '.6', '.6'))
+    plt.plot(xsmall, n_exp(xsmall, a, n), color='b', marker=',')
+    plt.plot(Xs, meters, mfc='k', mec='k', linestyle='None', marker='x')
     plt.savefig(f"shaded_area_{description}.png")
     plt.show()
 
-def stdev_for_dist(meters, rssi2d, description):
-    Ys_stdev = np.array([y.std() for y in rssi2d])
 
-    plt.ylabel('RSSI St. Dev. (dBm)')
+def stdev_for_dist(meters, rssi2d, description):
+    db_stdev = np.array([y.std() for y in rssi2d])
+    db_mean = np.array([y.mean() for y in rssi2d])
+
+    (a, a_stdev), (n, n_stdev), r2 = fit_curve_a_n_r2(meters[1:], rssi2d[1:])
+
+    deriv = np.log(10) / 10 / n * 10**((db_mean - a) / 10 / n)
+    m_stdev = np.sqrt(deriv * deriv + db_stdev * db_stdev)
+
+    plt.ylabel('Calculated Meters Standard Deviation (meters)')
     plt.xlabel('Distance (meters)')
-    plt.title('RSSI Standard Deviation vs Distance for ' + description)
+    plt.title('Calculated Meters St. Dev. vs Distance for ' + description)
+
+    plt.plot(meters, m_stdev, marker=',')
+
     plt.savefig(f"stdev_for_dist_{description}.png")
-    plt.plot(meters, Ys_stdev, marker = ',')
     plt.show()
 
-# (data, name) = all_phones[1]
-# box_plots_normal_ignore_0_with_fit(meters, data, name)
+
+# (data, name) = all_phones[5]
+# box_plots_normal_ignore_0_with_fit(meters[::-1], data, name)
 # stdev_for_dist(meters, data, name)
 # shaded_area(meters[::-1], data, name)
+# stdev_for_dist(meters[::-1], data, name)
