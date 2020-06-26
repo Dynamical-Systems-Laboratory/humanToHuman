8684da08beb8cbe06dbeb7f7b0cd74bd17408df9
Albert Liu <albertymliu@gmail.com>
2020-06-26 00:43:56 -0400

    added analysis stuffos

--- analysis/error.py
+++ analysis/error.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from scipy.optimize import curve_fit
+from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 import statistics
 import pickle
@@ -38,13 +39,28 @@ all_phones = [
     (i10_i8, 'iPhone 10 & iPhone 8'),
 ]
 
+def n_exp(x, a, n):
+    return 10 ** ((x - a) / 10 / n)
+
+def fit_curve_a_n_r2(meters, rssi2d):
+    x = [y.mean() for y in rssi2d]
+    (a, n), coef = curve_fit(n_exp, x, meters, p0 = [60,-30], xtol=0.001,
+                             maxfev=100000)
+    a_stdev = np.sqrt(coef[0,0])
+    n_stdev = np.sqrt(coef[1,1])
+
+    r2 = r2_score(n_exp(x, a, n), meters)
+
+    return (a, a_stdev), (n, n_stdev), r2
+
+
 def box_plots_normal_ignore_0_with_fit(meters, rssi2d, description, fit = True):
     ys = rssi2d[1:]
     Xmin = min(x.min() for x in rssi2d)
     Xmax = max(x.max() for x in rssi2d)
 
     plt.xlabel('RSSI (dBm)')
-    plt.ylabel('AB Distance (meters)')
+    plt.ylabel('Distance (meters)')
     plt.title('Distance vs Measured RSSI ' + description)
 
     plt.boxplot(rssi2d, positions = meters, vert = False)
@@ -54,52 +70,59 @@ def box_plots_normal_ignore_0_with_fit(meters, rssi2d, description, fit = True):
         plt.show()
         return
 
-    n_exp = lambda x, a, n: 10 ** ((x - a) / n)
-    (a, n), coef = curve_fit(n_exp, [y.mean() for y in ys], meters[1:],
-                             p0 = [60,-30],xtol=0.001,maxfev=100000)
-    n = n / 10
-
-    curve_exp = lambda x: 10 * n * np.log10(x) + a
+    (a, a_stdev), (n, n_stdev), r2 = fit_curve_a_n_r2(meters[1:], rssi2d[1:])
     xsmall = np.arange(Xmin, Xmax, .001)
-
-    plt.plot(xsmall, n_exp(xsmall, a, 10 * n), mfc='b', mec='b', marker=',')
+    plt.plot(xsmall, n_exp(xsmall, a, n), mfc='b', mec='b', marker=',')
     plt.ylim([-1, 10])
-    plt.figtext(-40, 8, f"a= {a}\nn= {n}\nr2= {coef[1,1]}")
+    text = f"""
+    a= {round(a, 2)} ± {round(a_stdev, 2)}
+    n= {round(n, 2)} ± {round(n_stdev, 2)}
+    r2= {round(r2, 2)}
+    """.strip().replace('    ', '')
+
+    plt.text(-60, 8, text)
     plt.savefig(f"box_plots_{description}.png")
     plt.show()
 
 def shaded_area(meters, rssi2d, description):
-    Xmin = min(x.min() for x in rssi2d)
-    Xmax = max(x.max() for x in rssi2d)
+    Xmin = min(x.mean() for x in rssi2d)
+    Xmax = max(x.mean() for x in rssi2d)
     Xs = np.array([x.mean() for x in rssi2d])
     Xs_stdev = np.array([x.std() for x in rssi2d])
-    n_exp = lambda x, a, n: 10 ** ((x - a) / n)
 
-    (a, n10), coef = curve_fit(n_exp, Xs[1:], meters[1:],
-                                xtol=0.001,maxfev=100000)
-    n = n10 / 10
+    (a, a_stdev), (n, n_stdev), r2 = fit_curve_a_n_r2(meters[1:], rssi2d[1:])
 
     plt.xlabel('RSSI (dBm)')
-    plt.ylabel('AB Distance (meters)')
+    plt.ylabel('Distance (meters)')
     plt.title('Distance vs Measured RSSI for ' + description)
 
-    curve_exp = lambda x: n10 * np.log10(x) + a
+    text = f"""
+    a= {round(a, 2)} ± {round(a_stdev, 2)}
+    n= {round(n, 2)} ± {round(n_stdev, 2)}
+    r2= {round(r2, 2)}
+    """.strip().replace('    ', '')
+    plt.text(-60, 8, text)
+
     xsmall = np.arange(Xmin, Xmax, .001)
-    shade_line = n_exp(Xs, a, n10)
+    shade_line = n_exp(Xs, a, n)
     plt.fill_between(Xs, Xs_stdev + shade_line, -Xs_stdev + shade_line,
                      color = ('.6', '.6','.6'))
-    plt.plot(xsmall, n_exp(xsmall, a, 10 * n), mfc='b', mec='b', marker=',')
+    plt.plot(xsmall, n_exp(xsmall, a, n), color = 'b', marker=',')
+    plt.plot(Xs, meters, mfc = 'k', mec='k', linestyle='None', marker='x')
     plt.savefig(f"shaded_area_{description}.png")
     plt.show()
 
-def stdev_for_dist(meters, rssi2d):
+def stdev_for_dist(meters, rssi2d, description):
     Ys_stdev = np.array([y.std() for y in rssi2d])
+
     plt.ylabel('RSSI St. Dev. (dBm)')
-    plt.xlabel('AB Distance (meters)')
-    plt.title('RSSI Standard Deviation vs Distance for Android & iPhone 10')
+    plt.xlabel('Distance (meters)')
+    plt.title('RSSI Standard Deviation vs Distance for ' + description)
+    plt.savefig(f"stdev_for_dist_{description}.png")
     plt.plot(meters, Ys_stdev, marker = ',')
     plt.show()
 
-
-(data, name) = all_phones[1]
-box_plots_normal_ignore_0_with_fit(meters, data, name)
+# (data, name) = all_phones[1]
+# box_plots_normal_ignore_0_with_fit(meters, data, name)
+# stdev_for_dist(meters, data, name)
+# shaded_area(meters[::-1], data, name)
