d34d273e633d23f203c83a3b7f12b76396357c93
Albert Liu <albertymliu@gmail.com>
2020-07-05 12:38:32 -0400

    fixed problems with postgres on linux server

--- main.go
+++ main.go
@@ -9,19 +9,30 @@ import (
 )
 
 type Flags struct {
-	Release  bool
-	Password *string
+	Release   bool
+	Password  *string
+	Dbconnstr string
+	Port      *string
 }
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	var flags Flags
+
+	flags.Dbconnstr = "user=humantohuman password=humantohuman sslmode=disable host=localhost dbname=humantohuman"
+
 	err := utils.ArgParseGlobal(&flags)
 	utils.FailIf(err, "Argument parsing failed")
-	utils.Print("%v", err)
+
+	port := ":8080"
+	if flags.Port != nil {
+		port = *flags.Port
+	}
+
 	if flags.Release && flags.Password == nil {
 		utils.Fail("password required for initial authentification!")
 	}
+
 	if flags.Release {
 		gin.SetMode(gin.ReleaseMode)
 		web.Release = true
@@ -32,18 +43,23 @@ func main() {
 
 	router := gin.Default()
 
-	database.ConnectToDb(database.DefaultURL)
-	database.ClearConnections()
-	router.GET("/clear", func(ctx *gin.Context) {
-		database.Clear()
-	})
+	database.ConnectToDb(flags.Dbconnstr)
 
-	router.GET("/experiment/:experiment/data.csv", web.GetCSV)
+	router.GET("/clear", func(c *gin.Context) {
+		web.JsonInfer(c, nil, database.Clear())
+	})
+	router.GET("/clearConnections", func(c *gin.Context) {
+		web.JsonInfer(c, nil, database.ClearConnections())
+	})
 	router.GET("/addExperiment", web.NewExperimentBrowser)
 	router.POST("/addExperiment", web.NewExperiment)
+
+	router.GET("/experiment/:experiment/data.csv", web.GetCSV)
 	router.GET("/experiment/:experiment/policy", web.GetPrivacyPolicy)
 	router.GET("/experiment/:experiment/description", web.GetDescription)
 	router.POST("/experiment/:experiment/addUser", web.NewUser)
 	router.POST("/experiment/:experiment/addConnections", web.AddConnectionsUnsafe)
-	router.Run(":8080")
+	router.POST("/experiment/:experiment/addConnectionsUnsafe", web.AddConnectionsUnsafe)
+
+	router.Run(port)
 }
