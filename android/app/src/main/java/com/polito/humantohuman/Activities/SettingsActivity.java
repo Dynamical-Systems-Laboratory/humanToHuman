a8c9570a68cd28c48b56046fbb70da49c354176a
Albert Liu <albertymliu@gmail.com>
2020-07-18 22:42:30 -0400

    fixed bug where pressing leave experiment twice would crash android

--- android/app/src/main/java/com/polito/humantohuman/Activities/SettingsActivity.java
+++ android/app/src/main/java/com/polito/humantohuman/Activities/SettingsActivity.java
@@ -50,8 +50,8 @@ public class SettingsActivity extends AppCompatActivity {
     });
 
     leaveExperimentButton.setOnClickListener((view) -> {
-      this.onResume();
       AppLogic.leaveExperiment(this);
+      this.onResume();
     });
   }
 
